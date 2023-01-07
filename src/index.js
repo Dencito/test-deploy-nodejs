ef39fe07925cabbdf56af4aec71001a14049dd6d
Denar Padilla <denarpadilla.lionel@gmail.com>
2023-01-07 17:31:50 -0300

    subiendo test de servidor en nodejs

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+
+const app = express()
+app.get("/", (req,res) => { 
+    res.send(`<h1>Bienvenido</h1>`)
+ })
+
+ app.get("/about", (req,res) => { 
+    res.send(`<h1>About</h1>`)
+ })
+
+app.listen(process.env.PORT || 3000)
+console.log("Server on port", process.env.PORT || 3000)
